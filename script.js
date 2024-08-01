70042b6623de74848a5870f1c4a81f68351cfc01
Adrian D'Costa <78658626+AnnonDomini@users.noreply.github.com>
2024-08-01 10:55:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// script.js
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const checkedCountSpan = document.getElementById('checkedCount');
+const percentSpan = document.getElementById('percent');
+const messageSpan = document.getElementById('message'); // Get the message element
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', countCheckboxes);
+});
+
+function countCheckboxes() {
+  let checkedCount = 0;
+  let percent = 0;
+
+  checkboxes.forEach(checkbox => {
+    if (checkbox.checked) {
+      checkedCount++;
+      percent = ((checkedCount / 50) * 100).toFixed(0);
+      message(percent); 
+    }
+  });
+
+  checkedCountSpan.textContent = checkedCount;
+  percentSpan.textContent = percent;
+}
+
+function message(percentage) {
+  let message;
+  if (percentage >= 95) {
+    message = "Damn Slut";
+  }else if(percentage>=85){
+    message = "Thats extreme, are you sure you didn't click any on accident";
+  }else if(percentage >= 50){
+    message = "Damn you do like getting dirty."
+  }else if(percentage >= 25){
+    message = "You've been getting a little adventurous"
+  }else{
+    message = "You're a pure soul, you make a preist jelous."
+  }
+  messageSpan.textContent = message; // Set the message text
+}
